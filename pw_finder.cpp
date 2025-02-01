5bc214a0fa374d0e0024f1ebbdffd31d929d76cd
Aaron Partridge <apartridge16@gmail.com>
2025-01-31 23:00:52 -0800

    added more functionality and .gitignore

--- pw_finder.cpp
+++ pw_finder.cpp
@@ -11,35 +11,66 @@ int main()
 {
     ifstream inFile("rockyou.txt");
 
-    string s;
-    string target = "bob123";
-    bool isFound = false;
-    int count = 0;
-
-    while (getline(inFile, s))
+    if (!inFile)
     {
-        count++;
+        cerr << "Error: Could not open file.\n";
+        return 1;
+    }
 
-        if (s == target)
+    char choice = 'y';
+    do
+    {
+        inFile.close();
+        inFile.open("rockyou.txt");
+
+        int count = 0;
+        string s = "";
+        string pw = "";
+        bool isFound = false;
+
+        cout << "\nEnter Password: ";
+        cin >> pw;
+
+        while (getline(inFile, s))
         {
-            isFound = true;
-            break;
+            count++;
+
+            if (s == pw)
+            {
+                isFound = true;
+                break;
+            }
         }
-    }
 
-    cout << "\n-----------------------------------------------------\n\n";
+        cout << "\n-----------------------------------------------------\n\n";
 
-    if (isFound)
-    {
-        cout << "FOUND\n\n";
-        cout << count << ": " << target;
-    }
-    else
-    {
-        cout << "CONGRATULATIONS, YOUR PASSWORD WASN'T FOUND";
-    }
+        if (isFound)
+        {
+            cout << "FOUND\n\n";
+            cout << count << ": " << pw;
+        }
+        else
+        {
+            cout << "CONGRATULATIONS, YOUR PASSWORD WASN'T FOUND";
+        }
 
-    cout << "\n\n-----------------------------------------------------\n";
+        cout << "\n\n-----------------------------------------------------\n\n";
+
+        cout << "Would you like to continue? (y/n): ";
+        cin >> choice;
+        choice = tolower(choice);
+        if (choice != 'y' && choice != 'n')
+        {
+            do
+            {
+                cout << "\nInvalid Input\n";
+                cout << "Would you like to continue? (y/n): ";
+                cin >> choice;
+                choice = tolower(choice);
+            } while (choice != 'y' && choice != 'n');
+        }
+
+    } while (choice == 'y');
 
     return 0;
 }
